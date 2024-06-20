3efae54e2ad25898ca9a899fef83ab033cf1e240
Gaganjot Singh <megaganjotsingh@gmail.com>
2024-06-20 22:19:33 +0530

    initial

---
+++ GSNotes/GSNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSNotesApp.swift
+//  GSNotes
+//
+//  Created by Gaganjot Singh on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GSNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
